c9da8d7a0423792287144dae77ab3c6f371f2ed0
kevinhades <you@example.com>
2019-03-27 14:54:18 +0800

    增加banner_type

--- app/service/banner.js
+++ app/service/banner.js
@@ -2,7 +2,7 @@ const Service = require('egg').Service;
 
 class BannerService extends Service {
     async list() {
-      let results = await this.app.mysql.query("select id, img_name, img_url, url_name, url, date_format(timestamp,'%Y-%m-%d %H:%i') as timestamp from video_banner where is_show = 1;");
+      let results = await this.app.mysql.query("select id, img_name, img_url, url_name, url_id, type_id, date_format(timestamp,'%Y-%m-%d %H:%i') as timestamp from video_banner where is_show = 1;");
       // select COLUMN_NAME from information_schema.COLUMNS where table_name = ?;
       return results;
     }

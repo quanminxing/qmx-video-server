4cf4aec7c875199682184f571d11a1193f8e6dfd
kevinhades <you@example.com>
2019-04-01 17:05:53 +0800

    添加样片是否在微信显示字段

--- app/service/video.js
+++ app/service/video.js
@@ -41,7 +41,7 @@ module.exports = app => {
         sql = 'select * from video_video where deleted = 0 ' + orderby;
         articles = yield app.mysql.query(sql + ' limit ? offset ?;', [ pageSize, (pageNum - 1) * pageSize ]);
       }else{
-        articles = yield app.mysql.query('select * from video_video where deleted = 0 order by timestamp desc limit ? offset ?;', [ pageSize, (pageNum - 1) * pageSize ]);
+        articles = yield app.mysql.query('select * from video_video where deleted = 0 and is_wechat = 1 order by timestamp asc limit ? offset ?;', [ pageSize, (pageNum - 1) * pageSize ]);
       }
        
       return articles;
@@ -49,22 +49,22 @@ module.exports = app => {
 
     // 获取某条信息
     * find(id) {
-      const article = yield app.mysql.query('select video_style.name as style_name, video_usage.name as usage_name, video_video.id as video_id ,video_video.name as video_name, video_video.price, business, time,video_video.platform_id,video_video.column_id,keystring, format,work_id, url, is_audio, is_text, is_model, is_show, is_scene, short_image,demo_description,demo_pic, video_category.name as category_name, video_platform.name as platform_name, video_column.name as column_name, video_video.timestamp,category_id, description from video_video LEFT JOIN video_category on video_video.category_id = video_category.id LEFT JOIN video_platform on video_video.platform_id = video_platform.id LEFT JOIN video_column on video_video.column_id = video_column.id LEFT JOIN video_usage on video_video.usage_id = video_usage.id LEFT JOIN video_style on video_video.style_id = video_style.id where video_video.id = ?;', [ id ]);
+      const article = yield app.mysql.query('select video_style.name as style_name, video_usage.name as usage_name, video_video.id as video_id ,video_video.name as video_name, video_video.price, business, time,video_video.platform_id,video_video.column_id,keystring, format,work_id, url, is_audio, is_text, is_model, is_show, is_scene, short_image,demo_description,demo_pic, video_category.name as category_name, video_platform.name as platform_name, video_column.name as column_name, video_video.timestamp,category_id, description from video_video LEFT JOIN video_category on video_video.category_id = video_category.id LEFT JOIN video_platform on video_video.platform_id = video_platform.id LEFT JOIN video_column on video_video.column_id = video_column.id LEFT JOIN video_usage on video_video.usage_id = video_usage.id LEFT JOIN video_style on video_video.style_id = video_style.id where video_video.id = ? and video_video.is_wechat = 1;', [ id ]);
 
       return article;
     }
 
     * listByHot(pageSize){
-      const articles = yield app.mysql.query('select * from video_hot LEFT JOIN video_video  on video_hot.video_id=video_video.id order by pv desc limit ?;', [ pageSize ]);
+      const articles = yield app.mysql.query('select * from video_hot LEFT JOIN video_video  on video_hot.video_id=video_video.id and video_video.is_wechat = 1 order by pv desc limit ?;', [ pageSize ]);
       return articles;
     }
 
     * listByRecommand(where, pageSize){
       let sql = '';
       if(where){
-        sql = `select * from video_video WHERE id >= (SELECT floor(RAND() * (SELECT MAX(id) FROM video_video))) ${where} ORDER BY id LIMIT 6`
+        sql = `select * from video_video WHERE id >= (SELECT floor(RAND() * (SELECT MAX(id) FROM video_video))) ${where} and video_video.is_wechat = 1 ORDER BY id LIMIT 6`
       }else{
-        sql = `select video_id as id,video_video.time ,video_video.name as name, video_video.short_image as short_image from video_recommand LEFT JOIN video_video on video_recommand.video_id=video_video.id ORDER BY video_recommand.id LIMIT 6`
+        sql = `select video_id as id,video_video.time ,video_video.name as name, video_video.short_image as short_image from video_recommand LEFT JOIN video_video on video_recommand.video_id=video_video.id where video_video.is_wechat = 1 ORDER BY video_recommand.id LIMIT 6`
       }
 
       const articles = yield app.mysql.query(sql);
@@ -77,7 +77,7 @@ module.exports = app => {
     * search(pageNum, pageSize, where) {
         let sql = 'select * from video_video where deleted = 0 and'
         sql += ' '+ where;
-        sql += ' order by timestamp desc limit ? offset ?;'
+        sql += 'and is_wechat = 1 order by timestamp asc limit ? offset ?;'
         const articles = yield app.mysql.query(sql, [pageSize, (pageNum - 1) * pageSize ]);
         return articles;
     }
@@ -85,14 +85,14 @@ module.exports = app => {
 
     // 搜索
     * searchByKeyword(pageNum, pageSize, keyword) {
-      let sql = `select id,name,url,short_image,price from video_video where name like '%` +keyword +`%' order by timestamp desc limit ? offset ?;`; 
+      let sql = `select id,name,url,short_image,price from video_video where name like '%` +keyword +`%' and is_wechat = 1 order by timestamp asc limit ? offset ?;`; 
       const articles = yield app.mysql.query(sql, [pageSize, (pageNum - 1) * pageSize ]);
       return articles;
     }
 
     // 总数
     * count(where) {
-        const count = yield app.mysql.query('select count(*) from video_video where ?', [where]);
+        const count = yield app.mysql.query('select count(*) from video_video where ? and is_wechat = 1', [where]);
 
         return count[0]['count(*)'];
     } 

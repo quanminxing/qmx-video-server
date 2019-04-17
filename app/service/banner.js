058d554935879290105a60c5cbb3d67ace34a175
kevinhades <you@example.com>
2019-04-17 14:44:25 +0800

    修复bannerbug

--- app/service/banner.js
+++ app/service/banner.js
@@ -3,14 +3,14 @@ const Service = require('egg').Service;
 class BannerService extends Service {
     async list(param) {
         const cond = param ? 'B.platform = ' + param : 'B.platform = ""'
-        let results = await this.app.mysql.query(`select A.*, date_format(A.timestamp,'%Y-%m-%d %H:%i') as timestamp from video_banner AS A left join video_banner_type AS B on A.type_id = B.id where A.is_show = 1 and is_del = false and ${cond};`);
+        let results = await this.app.mysql.query(`select A.*, date_format(A.timestamp,'%Y-%m-%d %H:%i') as timestamp from video_banner AS A left join video_banner_type AS B on A.type_id = B.id where A.is_show = 1  = false and ${cond};`);
         // select COLUMN_NAME from information_schema.COLUMNS where table_name = ?;
         return results;
     }
 
     async listAll(pageNum, pageSize) {
         try {
-            let results = await this.app.mysql.query(`select A.*, date_format(A.timestamp,"%Y-%m-%d %H:%i") as timestamp from video_banner  AS A left join video_banner_type AS B on A.type_id = B.id where is_del = false order by A.id desc limit ${pageSize} offset ${(pageNum - 1) * pageSize};`)
+            let results = await this.app.mysql.query(`select A.*, date_format(A.timestamp,"%Y-%m-%d %H:%i") as timestamp from video_banner  AS A left join video_banner_type AS B on A.type_id = B.id order by A.id desc limit ${pageSize} offset ${(pageNum - 1) * pageSize};`)
             return results;
         } catch (err) {
 

7ca9cd6808a529f0a51a965587e0b7ac3a5899f9
kevinhades <you@example.com>
2019-04-18 16:23:36 +0800

    修复价格调价查询视频bug

--- app/controller/video.js
+++ app/controller/video.js
@@ -407,7 +407,7 @@ class VideoController extends Controller {
 
 		let orderby = ` order by ${sidx} ${sord}`
 
-		let priceSql = price[0] ? ` and price >= ${price[0]}` : '' + price[1] ? ` and price <= ${price[1]}` : '';
+		let priceSql = (price[0] ? ` and price >= ${price[0]}` : '') + (price[1] ? ` and price <= ${price[1]}` : '');
 		if (_search === 'true') {
 			if (id) {
 				let sql = ' and VV.id = ' + id ;

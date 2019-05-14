6b19463d4f3d5309593f913b39c487b323d626d0
kevinhades <you@example.com>
2019-05-14 17:21:25 +0800

    role

--- config/role.js
+++ config/role.js
@@ -13,6 +13,8 @@ module.exports = app => {
   });
 
   app.role.use('staff', ctx => {
+    console.log(ctx)
+    
     const login = ctx.session && ctx.session.adminLogin ? ctx.session.adminLogin : false;
     const role = ctx.session && ctx.session.user ? ctx.session.user.position : '';
     if(login && (role == '员工' || role == '管理员')) {

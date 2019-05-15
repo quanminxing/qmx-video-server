cc9bc447f2eb6557eee3dcebe230675621031539
kevinhades <kevinhades95@gmail.com>
2019-05-15 17:34:35 +0800

    nonceStr

--- app/controller/pay.js
+++ app/controller/pay.js
@@ -66,7 +66,7 @@ class PayController extends Controller {
                 if(json.xml.return_code == 'SUCCESS' && json.xml.result_code == 'SUCCESS') {
                     //await this.service.pay.recordinsert(json.xml);
                     let timeStamp = Math.round(new Date() / 1000)
-                    let secondSignStr = appid + '&nonce_str=' + json.xml.nonce_str + '&package=prepay_id=' + json.xml.prepay_id + '&signType=MD5' + '&timeStamp=' + timeStamp;
+                    let secondSignStr = appid + '&nonceStr=' + json.xml.nonce_str + '&package=prepay_id=' + json.xml.prepay_id + '&signType=MD5' + '&timeStamp=' + timeStamp;
                     let secondSign = Util.getSign(secondSignStr, key);
                     that.ctx.body = {
                         status: 200,
